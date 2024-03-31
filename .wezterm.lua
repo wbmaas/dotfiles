3e3665c88d4180c24324c0e6922291a74f3525c1
William Maas <williambmaas@gmail.com>
2024-03-31 14:34:49 -0300

    Create .wezterm.lua

---
+++ .wezterm.lua
@@ -0,0 +1,17 @@
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
+local config = wezterm.config_builder()
+
+config.color_scheme = 'AdventureTime'
+config.font = wezterm.font 'Ubuntu Mono'
+config.font_size = 24
+-- config.color_scheme = 'Galaxy'
+config.color_scheme = 'Gruvbox dark, hard (base16)'
+config.use_fancy_tab_bar = false
+
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
+return config
