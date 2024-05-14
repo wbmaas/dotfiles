0a5c3e0a161d508ea3159382ff448d77b46827a8
William Maas <williambmaas@gmail.com>
2024-05-14 13:20:29 -0300

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,11 +2,11 @@ local wezterm = require 'wezterm'
 local mux = wezterm.mux
 local config = wezterm.config_builder()
 
-config.color_scheme = 'AdventureTime'
 config.font = wezterm.font 'Ubuntu Mono'
 config.font_size = 24
 -- config.color_scheme = 'Galaxy'
 -- config.color_scheme = 'Gruvbox dark, hard (base16)'
+-- config.color_scheme = 'AdventureTime'
 config.color_scheme = 'Tokyo Night'
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
